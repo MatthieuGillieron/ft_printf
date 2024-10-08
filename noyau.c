d612768b2fedcb1a4873991446b32acf6b47ede7
MatthieuGillieron <gillieron.matthieu@ik.me>
2024-10-08 14:02:00 +0200

    v2

--- noyau.c
+++ noyau.c
@@ -6,58 +6,68 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:11:10 by mg                #+#    #+#             */
-/*   Updated: 2024/09/26 19:25:11 by mg               ###   ########.fr       */
+/*   Updated: 2024/10/08 14:01:37 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_check_string(const char *type, va_list ap)
+int	ft_check_string(const char *type, va_list ap)
 {
 	int	i;
 	int avance;
+	int count_char;
 
 	i = 0;
+	count_char  = 0;
 	while (type[i])
 	{
 		if (type[i] == '%' && type[i + 1])
 		{
-			avance = ft_type_trouver(&type[i + 1], ap); // Passer après '%'
-			i += avance;  // Avancer du nombre de caractères retourné
+			avance = ft_type_trouver(&type[i + 1], ap);
+			i += avance;
+			count_char += avance; 
 		}
 		else
 		{
-			ft_putchar(type[i]);  // Afficher les caractères normaux
+			ft_putchar(type[i]);
+			count_char++;
 			i++;
 		}
 	}
+	return (count_char);
 }
 
 int	ft_type_trouver(const char *type, va_list ap)
 {
-	// Pas besoin de gérer explicitement le '%' ici, car on passe déjà `&type[i + 1]`
 	if (*type == '%')
-		ft_print_char('%');  // Affiche un pourcentage littéral
+		ft_print_char('%');
 	else if (*type == 'c')
-		ft_print_char(va_arg(ap, int));  // Gérer un caractère
+		ft_print_char(va_arg(ap, int));
 	else if (*type == 's')
-		ft_print_str(va_arg(ap, char *));  // Gérer une chaîne de caractères
+		ft_print_str(va_arg(ap, char *));
 	else if (*type == 'd' || *type == 'i')
-		ft_print_digit(va_arg(ap, int));  // Gérer un entier (signed)
+		ft_print_digit(va_arg(ap, int));
+	else if (*type == 'u')
+		ft_print_unsigned(va_arg(ap, unsigned int));
+	else if (*type == 'x')
+		ft_print_hex_min(va_arg(ap, unsigned int));
+	else if (*type == 'X')
+		ft_print_hex_maj(va_arg(ap, unsigned int));
+	else if (*type == 'p')
+		ft_print_adresse(va_arg(ap, void *));
 	else
-		return 0;  // Aucun spécificateur trouvé
+		return 0;
+	return 1;
+} 
 
-	return 1;  // On retourne 1 pour avancer après le spécificateur
-}
 
 int ft_printf(const char *format, ...)
 {
-    va_list ap;  // Créer une liste d'arguments variadiques
-    va_start(ap, format);  // Initialiser la liste avec les arguments qui suivent 'format'
-
-    // Appeler la fonction de parsing (parcourt le format et affiche chaque argument)
-    ft_check_string(format, ap);
-    
-    va_end(ap);  // Terminer l'utilisation des arguments variadiques
-    return 0;  // Pour l'instant, on retourne 0, mais on pourrait retourner le nombre de caractères imprimés par exemple.
+    va_list ap;
+    va_start(ap, format);
+	int	count_char = 0;
+    count_char = ft_check_string(format, ap);
+    va_end(ap);
+    return count_char;
 }
--- utilitaires.c
+++ utilitaires.c
@@ -6,13 +6,13 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:08:13 by mg                #+#    #+#             */
-/*   Updated: 2024/09/26 19:25:27 by mg               ###   ########.fr       */
+/*   Updated: 2024/10/08 11:08:43 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar(char c) // int c -> pourquoi?
+void	ft_putchar(int c)
 {
 	write(1, &c, 1);
 }
